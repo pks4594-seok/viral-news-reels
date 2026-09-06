09bc694fe0376bfe0a84c0095be547e180cc8ff7
박기석 <pks4594@gmail.com>
2026-09-06 01:20:12 +0000

    genspark auto-backup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         applicationId = "com.trendreelstudio.creator"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
